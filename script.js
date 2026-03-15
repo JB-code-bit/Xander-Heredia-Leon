edff9174198f3b58ae5f1bd5f6191d7f3a553866
JB-code-bit <codebitmail@gmail.com>
2026-03-15 13:48:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const gallery=document.getElementById("gallery")
 
+for(let i=0;i<200;i++){
+
+const photo=document.createElement("div")
+
+photo.className="photo"
+
+photo.innerHTML=`
+
+<img src="">
+
+<img class="pin" src="images/pin.png">
+
+`
+
+gallery.appendChild(photo)
+
+}
